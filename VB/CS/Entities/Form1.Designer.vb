39b3c68edf9f646b7c675ae6c9a41fa83bda2d1f
DevExpressExampleBot <fakeEmail>
2020-08-04 16:11:05 +0300

    Source auto update [skip ci]

--- VB/CS/Entities/Form1.Designer.vb
+++ VB/CS/Entities/Form1.Designer.vb
@@ -1,202 +1,194 @@
 ﻿Namespace Entities
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#Region "Windows Form Designer generated code"
+        #Region "Windows Form Designer generated code"
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Dim gridLevelNode1 As New DevExpress.XtraGrid.GridLevelNode()
-			Me.gridView2 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.colAddress = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colCity = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colCompanyName = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colContactName = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colContactTitle = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colCountry = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colCustomerID = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colFax = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colPhone = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colPostalCode = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.colRegion = New DevExpress.XtraGrid.Columns.GridColumn()
-			CType(Me.gridView2, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridView2
-			' 
-			Me.gridView2.GridControl = Me.gridControl1
-			Me.gridView2.Name = "gridView2"
-			Me.gridView2.OptionsView.ShowGroupPanel = False
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			gridLevelNode1.LevelTemplate = Me.gridView2
-			gridLevelNode1.RelationName = "Orders"
-			Me.gridControl1.LevelTree.Nodes.AddRange(New DevExpress.XtraGrid.GridLevelNode() { gridLevelNode1})
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.ShowOnlyPredefinedDetails = True
-			Me.gridControl1.Size = New System.Drawing.Size(878, 499)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1, Me.gridView2})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colAddress, Me.colCity, Me.colCompanyName, Me.colContactName, Me.colContactTitle, Me.colCountry, Me.colCustomerID, Me.colFax, Me.colPhone, Me.colPostalCode, Me.colRegion})
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.gridView1.MasterRowGetRelationName += new DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationNameEventHandler(this.gridView1_MasterRowGetRelationName);
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.gridView1.MasterRowGetChildList += new DevExpress.XtraGrid.Views.Grid.MasterRowGetChildListEventHandler(this.gridView1_MasterRowGetChildList);
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.gridView1.MasterRowEmpty += new DevExpress.XtraGrid.Views.Grid.MasterRowEmptyEventHandler(this.gridView1_MasterRowEmpty);
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.gridView1.MasterRowGetRelationCount += new DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationCountEventHandler(this.gridView1_MasterRowGetRelationCount);
-			' 
-			' colAddress
-			' 
-			Me.colAddress.Caption = "Address"
-			Me.colAddress.FieldName = "Address"
-			Me.colAddress.Name = "colAddress"
-			Me.colAddress.Visible = True
-			Me.colAddress.VisibleIndex = 0
-			' 
-			' colCity
-			' 
-			Me.colCity.Caption = "City"
-			Me.colCity.FieldName = "City"
-			Me.colCity.Name = "colCity"
-			Me.colCity.Visible = True
-			Me.colCity.VisibleIndex = 1
-			' 
-			' colCompanyName
-			' 
-			Me.colCompanyName.Caption = "CompanyName"
-			Me.colCompanyName.FieldName = "CompanyName"
-			Me.colCompanyName.Name = "colCompanyName"
-			Me.colCompanyName.Visible = True
-			Me.colCompanyName.VisibleIndex = 2
-			' 
-			' colContactName
-			' 
-			Me.colContactName.Caption = "ContactName"
-			Me.colContactName.FieldName = "ContactName"
-			Me.colContactName.Name = "colContactName"
-			Me.colContactName.Visible = True
-			Me.colContactName.VisibleIndex = 3
-			' 
-			' colContactTitle
-			' 
-			Me.colContactTitle.Caption = "ContactTitle"
-			Me.colContactTitle.FieldName = "ContactTitle"
-			Me.colContactTitle.Name = "colContactTitle"
-			Me.colContactTitle.Visible = True
-			Me.colContactTitle.VisibleIndex = 4
-			' 
-			' colCountry
-			' 
-			Me.colCountry.Caption = "Country"
-			Me.colCountry.FieldName = "Country"
-			Me.colCountry.Name = "colCountry"
-			Me.colCountry.Visible = True
-			Me.colCountry.VisibleIndex = 5
-			' 
-			' colCustomerID
-			' 
-			Me.colCustomerID.Caption = "CustomerID"
-			Me.colCustomerID.FieldName = "CustomerID"
-			Me.colCustomerID.Name = "colCustomerID"
-			Me.colCustomerID.Visible = True
-			Me.colCustomerID.VisibleIndex = 6
-			' 
-			' colFax
-			' 
-			Me.colFax.Caption = "Fax"
-			Me.colFax.FieldName = "Fax"
-			Me.colFax.Name = "colFax"
-			Me.colFax.Visible = True
-			Me.colFax.VisibleIndex = 7
-			' 
-			' colPhone
-			' 
-			Me.colPhone.Caption = "Phone"
-			Me.colPhone.FieldName = "Phone"
-			Me.colPhone.Name = "colPhone"
-			Me.colPhone.Visible = True
-			Me.colPhone.VisibleIndex = 8
-			' 
-			' colPostalCode
-			' 
-			Me.colPostalCode.Caption = "PostalCode"
-			Me.colPostalCode.FieldName = "PostalCode"
-			Me.colPostalCode.Name = "colPostalCode"
-			Me.colPostalCode.Visible = True
-			Me.colPostalCode.VisibleIndex = 9
-			' 
-			' colRegion
-			' 
-			Me.colRegion.Caption = "Region"
-			Me.colRegion.FieldName = "Region"
-			Me.colRegion.Name = "colRegion"
-			Me.colRegion.Visible = True
-			Me.colRegion.VisibleIndex = 10
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(878, 499)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.gridView2, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Dim gridLevelNode1 As New DevExpress.XtraGrid.GridLevelNode()
+            Me.gridView2 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.colAddress = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colCity = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colCompanyName = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colContactName = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colContactTitle = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colCountry = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colCustomerID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colFax = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colPhone = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colPostalCode = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colRegion = New DevExpress.XtraGrid.Columns.GridColumn()
+            DirectCast(Me.gridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridView2
+            ' 
+            Me.gridView2.GridControl = Me.gridControl1
+            Me.gridView2.Name = "gridView2"
+            Me.gridView2.OptionsView.ShowGroupPanel = False
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            gridLevelNode1.LevelTemplate = Me.gridView2
+            gridLevelNode1.RelationName = "Orders"
+            Me.gridControl1.LevelTree.Nodes.AddRange(New DevExpress.XtraGrid.GridLevelNode() { gridLevelNode1})
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.ShowOnlyPredefinedDetails = True
+            Me.gridControl1.Size = New System.Drawing.Size(878, 499)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1, Me.gridView2})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colAddress, Me.colCity, Me.colCompanyName, Me.colContactName, Me.colContactTitle, Me.colCountry, Me.colCustomerID, Me.colFax, Me.colPhone, Me.colPostalCode, Me.colRegion})
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            ' 
+            ' colAddress
+            ' 
+            Me.colAddress.Caption = "Address"
+            Me.colAddress.FieldName = "Address"
+            Me.colAddress.Name = "colAddress"
+            Me.colAddress.Visible = True
+            Me.colAddress.VisibleIndex = 0
+            ' 
+            ' colCity
+            ' 
+            Me.colCity.Caption = "City"
+            Me.colCity.FieldName = "City"
+            Me.colCity.Name = "colCity"
+            Me.colCity.Visible = True
+            Me.colCity.VisibleIndex = 1
+            ' 
+            ' colCompanyName
+            ' 
+            Me.colCompanyName.Caption = "CompanyName"
+            Me.colCompanyName.FieldName = "CompanyName"
+            Me.colCompanyName.Name = "colCompanyName"
+            Me.colCompanyName.Visible = True
+            Me.colCompanyName.VisibleIndex = 2
+            ' 
+            ' colContactName
+            ' 
+            Me.colContactName.Caption = "ContactName"
+            Me.colContactName.FieldName = "ContactName"
+            Me.colContactName.Name = "colContactName"
+            Me.colContactName.Visible = True
+            Me.colContactName.VisibleIndex = 3
+            ' 
+            ' colContactTitle
+            ' 
+            Me.colContactTitle.Caption = "ContactTitle"
+            Me.colContactTitle.FieldName = "ContactTitle"
+            Me.colContactTitle.Name = "colContactTitle"
+            Me.colContactTitle.Visible = True
+            Me.colContactTitle.VisibleIndex = 4
+            ' 
+            ' colCountry
+            ' 
+            Me.colCountry.Caption = "Country"
+            Me.colCountry.FieldName = "Country"
+            Me.colCountry.Name = "colCountry"
+            Me.colCountry.Visible = True
+            Me.colCountry.VisibleIndex = 5
+            ' 
+            ' colCustomerID
+            ' 
+            Me.colCustomerID.Caption = "CustomerID"
+            Me.colCustomerID.FieldName = "CustomerID"
+            Me.colCustomerID.Name = "colCustomerID"
+            Me.colCustomerID.Visible = True
+            Me.colCustomerID.VisibleIndex = 6
+            ' 
+            ' colFax
+            ' 
+            Me.colFax.Caption = "Fax"
+            Me.colFax.FieldName = "Fax"
+            Me.colFax.Name = "colFax"
+            Me.colFax.Visible = True
+            Me.colFax.VisibleIndex = 7
+            ' 
+            ' colPhone
+            ' 
+            Me.colPhone.Caption = "Phone"
+            Me.colPhone.FieldName = "Phone"
+            Me.colPhone.Name = "colPhone"
+            Me.colPhone.Visible = True
+            Me.colPhone.VisibleIndex = 8
+            ' 
+            ' colPostalCode
+            ' 
+            Me.colPostalCode.Caption = "PostalCode"
+            Me.colPostalCode.FieldName = "PostalCode"
+            Me.colPostalCode.Name = "colPostalCode"
+            Me.colPostalCode.Visible = True
+            Me.colPostalCode.VisibleIndex = 9
+            ' 
+            ' colRegion
+            ' 
+            Me.colRegion.Caption = "Region"
+            Me.colRegion.FieldName = "Region"
+            Me.colRegion.Name = "colRegion"
+            Me.colRegion.Visible = True
+            Me.colRegion.VisibleIndex = 10
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(878, 499)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            DirectCast(Me.gridView2, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
 
-		End Sub
+        End Sub
 
-		#End Region
+        #End Region
 
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private colAddress As DevExpress.XtraGrid.Columns.GridColumn
-		Private colCity As DevExpress.XtraGrid.Columns.GridColumn
-		Private colCompanyName As DevExpress.XtraGrid.Columns.GridColumn
-		Private colContactName As DevExpress.XtraGrid.Columns.GridColumn
-		Private colContactTitle As DevExpress.XtraGrid.Columns.GridColumn
-		Private colCountry As DevExpress.XtraGrid.Columns.GridColumn
-		Private colCustomerID As DevExpress.XtraGrid.Columns.GridColumn
-		Private colFax As DevExpress.XtraGrid.Columns.GridColumn
-		Private colPhone As DevExpress.XtraGrid.Columns.GridColumn
-		Private colPostalCode As DevExpress.XtraGrid.Columns.GridColumn
-		Private colRegion As DevExpress.XtraGrid.Columns.GridColumn
-		Private gridView2 As DevExpress.XtraGrid.Views.Grid.GridView
-	End Class
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+        Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private colAddress As DevExpress.XtraGrid.Columns.GridColumn
+        Private colCity As DevExpress.XtraGrid.Columns.GridColumn
+        Private colCompanyName As DevExpress.XtraGrid.Columns.GridColumn
+        Private colContactName As DevExpress.XtraGrid.Columns.GridColumn
+        Private colContactTitle As DevExpress.XtraGrid.Columns.GridColumn
+        Private colCountry As DevExpress.XtraGrid.Columns.GridColumn
+        Private colCustomerID As DevExpress.XtraGrid.Columns.GridColumn
+        Private colFax As DevExpress.XtraGrid.Columns.GridColumn
+        Private colPhone As DevExpress.XtraGrid.Columns.GridColumn
+        Private colPostalCode As DevExpress.XtraGrid.Columns.GridColumn
+        Private colRegion As DevExpress.XtraGrid.Columns.GridColumn
+        Private gridView2 As DevExpress.XtraGrid.Views.Grid.GridView
+    End Class
 End Namespace
 
