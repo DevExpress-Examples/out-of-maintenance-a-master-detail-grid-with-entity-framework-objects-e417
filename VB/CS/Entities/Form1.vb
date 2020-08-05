28122857d6565fb13f4b11a7bd3935cc19b1f364
DevExpressExampleBot <fakeEmail>
2020-08-05 10:46:57 +0300

    Source auto update [skip ci]

--- VB/CS/Entities/Form1.vb
+++ VB/CS/Entities/Form1.vb
@@ -8,39 +8,36 @@ Imports System.Text
 Imports System.Windows.Forms
 Imports System.Data.Objects
 Imports System.Data.Objects.DataClasses
-Imports DevExpress.XtraGrid.Views.Grid
 
 Namespace Entities
-	Partial Public Class Form1
-		Inherits Form
+    Partial Public Class Form1
+        Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-			Dim northwind As New NorthwindEntities()
-			Dim customersQuery As ObjectQuery(Of Customers) = northwind.Customers.Include("Orders")
-			gridControl1.DataSource = New BindingSource(customersQuery, "")
+        Public Sub New()
+            InitializeComponent()
+            Dim northwind As New NorthwindEntities()
+            Dim customersQuery As ObjectQuery(Of Customers) = northwind.Customers.Include("Orders")
+            gridControl1.DataSource = New BindingSource(customersQuery, "")
 
-			gridView1.OptionsSelection.MultiSelect = True
-		End Sub
+            gridView1.OptionsSelection.MultiSelect = True
+        End Sub
 
-		Private Sub gridView1_MasterRowEmpty(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.MasterRowEmptyEventArgs) Handles gridView1.MasterRowEmpty
-			Dim view As GridView = TryCast(sender, GridView)
-			Dim c As Customers = CType(view.GetRow(e.RowHandle), Customers)
-			e.IsEmpty = c.Orders.Count = 0
-		End Sub
+        Private Sub gridView1_MasterRowEmpty(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.MasterRowEmptyEventArgs) Handles gridView1.MasterRowEmpty
+            Dim c As Customers = CType(gridView1.GetRow(e.RowHandle), Customers)
+            e.IsEmpty = c.Orders.Count = 0
+        End Sub
 
-		Private Sub gridView1_MasterRowGetRelationCount(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationCountEventArgs) Handles gridView1.MasterRowGetRelationCount
-			e.RelationCount = 1
-		End Sub
+        Private Sub gridView1_MasterRowGetRelationCount(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationCountEventArgs) Handles gridView1.MasterRowGetRelationCount
+            e.RelationCount = 1
+        End Sub
 
-		Private Sub gridView1_MasterRowGetRelationName(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationNameEventArgs) Handles gridView1.MasterRowGetRelationName
-			e.RelationName = "Orders"
-		End Sub
+        Private Sub gridView1_MasterRowGetRelationName(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.MasterRowGetRelationNameEventArgs) Handles gridView1.MasterRowGetRelationName
+            e.RelationName = "Orders"
+        End Sub
 
-		Private Sub gridView1_MasterRowGetChildList(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.MasterRowGetChildListEventArgs) Handles gridView1.MasterRowGetChildList
-			Dim view As GridView = TryCast(sender, GridView)
-			Dim c As Customers = CType(view.GetRow(e.RowHandle), Customers)
-			e.ChildList = c.Orders.ToList()
-		End Sub
-	End Class
+        Private Sub gridView1_MasterRowGetChildList(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.MasterRowGetChildListEventArgs) Handles gridView1.MasterRowGetChildList
+            Dim c As Customers = CType(gridView1.GetRow(e.RowHandle), Customers)
+            e.ChildList = New BindingSource(c, "Orders")
+        End Sub
+    End Class
 End Namespace
